74b577e44eaedab12a619e30f0b78977ed704d7b
joaopasquini <joaopasquinijr@hotmail.com>
2021-06-12 17:11:06 -0300

    primeiro commit, criacao do projeto para API Rest

---
+++ boletim/src/test/java/sere/com/br/boletim/BoletimApplicationTests.java
@@ -0,0 +1,13 @@
+package sere.com.br.boletim;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BoletimApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
